dc48c7f9f3d43eb54c0cf7b43588c94e95d28110
Anton Veldanov <anton.veldanov@gmail.com>
2020-12-22 13:34:52 -0800

    first commit

---
+++ ShareItApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ShareItApp
+//
+//  Created by Veldanov, Anton on 12/22/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
