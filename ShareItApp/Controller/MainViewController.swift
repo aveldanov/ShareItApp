006d41d879bb7bbf9c4994667a11810864d3669f
Anton Veldanov <anton.veldanov@gmail.com>
2020-12-24 21:25:57 -0800

    iOS

--- ShareItApp/Controller/MainViewController.swift
+++ ShareItApp/Controller/MainViewController.swift
@@ -14,13 +14,33 @@ enum PostCategory: String {
     case popular = "popular"
 }
 
-class MainViewController: UIViewController {
+class MainViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    
+//MARK: - Outlets
+    
+    
+    @IBOutlet weak var segmentControl: UISegmentedControl!
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
 
     }
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell
+    }
 
 }
 
