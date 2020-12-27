da87ff7107618ddd59294aa9b6c6b30e28ef5ebb
Anton Veldanov <anton.veldanov@gmail.com>
2020-12-26 22:17:13 -0800

    iOS

--- ShareItApp/Controller/LoginViewController.swift
+++ ShareItApp/Controller/LoginViewController.swift
@@ -8,7 +8,16 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
+    
+    //MARK: - Outlets
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var createUserButton: UIButton!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
