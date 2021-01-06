304628c3d386de090d003c5232da291feaf6c385
Anton Veldanov <anton.veldanov@gmail.com>
2021-01-05 22:16:38 -0800

    iOS

---
+++ ShareItApp/Controller/Cells/CommentTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  CommentTableViewCell.swift
+//  ShareItApp
+//
+//  Created by Veldanov, Anton on 1/5/21.
+//
+
+import UIKit
+
+class CommentTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+
+
+}
