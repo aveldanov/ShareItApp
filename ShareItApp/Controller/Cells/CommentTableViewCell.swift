671f8b59730dc4e7c9c8820a05578fe0e3e00ca7
Anton Veldanov <anton.veldanov@gmail.com>
2021-01-05 22:18:48 -0800

    iOS

--- ShareItApp/Controller/Cells/CommentTableViewCell.swift
+++ ShareItApp/Controller/Cells/CommentTableViewCell.swift
@@ -8,7 +8,15 @@
 import UIKit
 
 class CommentTableViewCell: UITableViewCell {
-
+//MARK: - Outlets
+    
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var timeStampLabel: UILabel!
+    @IBOutlet weak var commentLabel: UILabel!
+    
+    
+    
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
