6c78723100e2fb131645059c744f6e4456814c60
Anton Veldanov <anton.veldanov@gmail.com>
2021-01-05 21:48:41 -0800

    iOS

---
+++ ShareItApp/Controller/CommentsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  CommentsViewController.swift
+//  ShareItApp
+//
+//  Created by Veldanov, Anton on 1/5/21.
+//
+
+import UIKit
+
+class CommentsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
